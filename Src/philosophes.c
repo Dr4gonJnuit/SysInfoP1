a033553449a14d13609330f6542f6894f70e42b0
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-11-15 00:06:06 +0100

    Continuation du projet

--- Src/philosophes.c
+++ Src/philosophes.c
@@ -17,24 +17,30 @@ void mange(int id)
 
 void *philosophe(void *arg)
 {
-    int *id = (int *)arg;
-    int left = *id;
+    int id = *((int *)arg);
+    int left = id;
     int right = (left + 1) % PHILOSOPHES;
-    while (true)
+
+    for (size_t i = 0; i < CYCLES; i++)
     {
-        printf("Philosophe [%d] pense\n", *id);
+        printf("Philosophe [%d] pense\n", id);
 
         sem_wait(&baguette[left]);
-        printf("Philosophe [%d] possède baguette gauche [%d]\n", *id, left);
+        printf("Philosophe [%d] possède baguette gauche [%d]\n", id, left);
         sem_wait(&baguette[right]);
-        printf("Philosophe [%d] possède baguette droite [%d]\n", *id, right);
+        printf("Philosophe [%d] possède baguette droite [%d]\n", id, right);
 
         mange(*id);
-        
+
         sem_post(&baguette[left]);
-        printf("Philosophe [%d] a libéré baguette gauche [%d]\n", *id, left);
+        printf("Philosophe [%d] a libéré baguette gauche [%d]\n", id, left);
         sem_post(&baguette[right]);
-        printf("Philosophe [%d] a libéré baguette droite [%d]\n", *id, right);
+        printf("Philosophe [%d] a libéré baguette droite [%d]\n", id, right);
     }
     return (NULL);
+}
+
+void run_philosophes()
+{
+    /* Code Here */
 }
---
+++ main.c
@@ -0,0 +1,52 @@
+#include "headers/includes.h"
+
+// Pas pour tout de suite
+/*
+typedef struct
+{
+    FILE *input_file;
+    FILE *output_stream;
+} args_t;
+
+
+void usage(char *prog_name)
+{
+    return;
+}
+
+int parse_args(args_t *args, int argc, char const **argv)
+{
+    memset(args, 0, sizeof(args_t));
+
+    args->output_stream = stdout;
+    args->input_file = NULL;
+    int opt;
+    while ((opt = getopt(argc, argv, "hn:vf:")) != -1)
+    {
+        switch (opt)
+        {
+        case '?':
+        case 'h':
+            usage(argv[0]);
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char const **argv)
+{
+    args_t args;
+    int err = parse_args(&args, argc, argv);
+    if (err == -1)
+    {
+        exit(EXIT_FAILURE);
+    }
+    else if (err == 1)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    return 0;
+}
+*/
