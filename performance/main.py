d06bf40236139cfe29954cc0d93bd60bccf764ba
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-11-23 18:19:16 +0100

    Script de lancement et affichage du graphe

---
+++ performance/main.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Read the .csv file
+data = pd.read_csv('performance/temps_execution.csv')
+
+# Calculate the time taken for each code execution
+time_philo = data['temps_philo']
+
+# Plot the time taken on the y-axis, starting from 0
+plt.plot(range(len(time_philo)), time_philo)
+plt.ylim(0, max(time_philo))  # Set the y-axis limits
+
+# Display the mean and standard deviation
+mean_time = time_philo.mean()
+std_dev = time_philo.std()
+plt.axhline(mean_time, color='r', linestyle='--', label='Mean')
+plt.axhline(mean_time + std_dev, color='g', linestyle='--', label='Mean + Std Dev')
+plt.axhline(mean_time - std_dev, color='g', linestyle='--', label='Mean - Std Dev')
+
+# Add labels and legend
+plt.xlabel('Execution')
+plt.ylabel('Time Taken')
+plt.legend()
+
+# Show the plot
+plt.show()
