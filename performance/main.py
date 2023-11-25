398037bd5a5ae80d4d1baa1151c4842af39c005e
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-11-25 11:54:49 +0100

    Affichage des graphes

--- performance/main.py
+++ performance/main.py
@@ -1,26 +1,36 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('temps_execution.csv')
+def graph_window(size, name):
+    plt.figure(figsize=size)
+    for i in range(0, len(name)):
+        graph_plot(name[i], len(name) * 100 + 10 + i + 1)
+    plt.show()
 
-# Calculate the time taken for each code execution
-time_philo = data['temps_philo']
+def graph_plot(name, subplot_position=111):
+    plt.subplot(subplot_position)
+    
+    data_read = pd.read_csv(name, index_col='n')
+    
+    nbr_threads = data_read.index
+    
+    data_means = data_read.mean(axis=1)
+    data_std = data_read.std(axis=1)
+    
+    plt.plot(data_means.index, data_means, label='Mean Time')
+    plt.fill_between(data_means.index, data_means - data_std, data_means + data_std, alpha=0.2, label='Standard Deviation')
+    plt.xticks(data_means.index, nbr_threads)
+    plt.ylim(0, max(data_means) + max(data_std))
+    plt.xlabel('Number of Threads')
+    plt.ylabel('Time Taken')
+    plt.legend()
+    plt.title(name.split('/')[1].split('.')[0])
+    
+    plt.grid(True)
+    
+    # Remove the duplicate plt.show() call
+    # plt.show()
 
-# Plot the time taken on the y-axis, starting from 0
-plt.plot(range(len(time_philo)), time_philo)
-plt.ylim(0, max(time_philo))  # Set the y-axis limits
-
-# Display the mean and standard deviation
-mean_time = time_philo.mean()
-std_dev = time_philo.std()
-plt.axhline(mean_time, color='r', linestyle='--', label='Mean')
-plt.axhline(mean_time + std_dev, color='g', linestyle='--', label='Mean + Std Dev')
-plt.axhline(mean_time - std_dev, color='g', linestyle='--', label='Mean - Std Dev')
-
-# Add labels and legend
-plt.xlabel('Execution')
-plt.ylabel('Time Taken')
-plt.legend()
-
-# Show the plot
-plt.show()
+if __name__ == '__main__':
+    file_to_graph = ['csv/ingi_temps_philo.csv', 'csv/ingi_temps_rw.csv']
+    graph_window((100, 50), file_to_graph)
