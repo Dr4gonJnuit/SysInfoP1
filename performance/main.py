fd627dfe3c053b713024c39a62a36c4e04b3384e
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-11-29 15:04:07 +0100

    Changement du lien d'un fichier et image des résultats

--- performance/main.py
+++ performance/main.py
@@ -23,6 +23,6 @@ def graph_plot(name, subplot_position=111, title='', show=False):
         plt.show()
 
 if __name__ == '__main__':
-    file_to_graph = ['csv/ingi_temps_philo.csv', 'csv/temps_pc.csv','csv/ingi_temps_rw.csv']
+    file_to_graph = ['csv/ingi_temps_philo.csv', 'csv/ingi_temps_pc.csv','csv/ingi_temps_rw.csv']
     title_names = ['Temps d\'exécution du problème des philosophes', 'Temps d\'exécution du problème des producteurs-consomateurs','Temps d\'exécution du problème des lecteurs-écrivains']
-    graph_window((9, 4), file_to_graph, title_names)
+    graph_window((90, 40), file_to_graph, title_names)
