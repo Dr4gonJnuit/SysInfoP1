f62ec6fda87cf0e19d6e5a07e09c988e1bb1b1d9
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-12-08 21:31:16 +0100

    Modif python et affichage des graphes

--- performance/main.py
+++ performance/main.py
@@ -1,27 +1,94 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def convert_time(time_string):
-    minutes, seconds = time_string.split(':')
-    seconds, milliseconds = seconds.split('.')
+    minutes, seconds = time_string.split(":")
+    seconds, milliseconds = seconds.split(".")
     total_time = int(minutes) * 60 * 1000 + int(seconds) * 1000 + int(milliseconds)
     return total_time
 
-def graph_line(file, name, show=False):
-    data = pd.read_csv(file, index_col='n').applymap(convert_time)
-    
-    print(data)
-    
-    plt.plot(data.T, data.index)
-    
-    if show:
-        plt.show
 
-if __name__ == '__main__':
-    csv_to_graph = ['performance/csv/Ingi_temps_philo_POSIX.csv', \
-                     'performance/csv/Ingi_temps_philo_TAS.csv', \
-                     'performance/csv/Ingi_temps_philo_TATAS.csv']
-    name_of_graph = ['Temps philo POSIX', 'Temps philo TAS', 'Temps philo TATAS']
-    
-    graph_line(csv_to_graph[0], name_of_graph[0], True)
+def graph_line(files, names, show=False):
+    for file in files:
+        # Read data from csv file
+        data = pd.read_csv(file, index_col="n").map(convert_time)
+
+        # Calculate mean and standard deviation
+        mean = data.mean(axis=1)
+        std = data.std(axis=1)
+
+        # Plot the graph
+        plt.errorbar(
+            data.index,
+            mean,
+            yerr=std,
+            fmt="-o",
+            label=names,
+            markersize=3,
+            linewidth=1,
+            capsize=6,
+        )
+        plt.legend(names, loc="upper left")
+
+    # Informations about the graph
+    plt.title(names[-1])
+    plt.xlabel("Number of Threads (n)")
+    plt.ylabel("Execution Time (ms)")
+    plt.grid(True)
+
+    # Force y-axis to start at 0
+    plt.ylim(bottom=0)
+
+    # Force x-axis to display index values
+    plt.xticks(data.index)
+
+    if show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    # Paths to csv files
+    csv_to_graph_philo = [
+        "performance/csv/Ingi_temps_philo_POSIX.csv",
+        "performance/csv/Ingi_temps_philo_TAS.csv",
+        "performance/csv/Ingi_temps_philo_TATAS.csv",
+    ]
+
+    csv_to_graph_prod_cons = [
+        "performance/csv/Ingi_temps_prod_cons_POSIX.csv",
+        "performance/csv/Ingi_temps_prod_cons_TAS.csv",
+        "performance/csv/Ingi_temps_prod_cons_TATAS.csv",
+    ]
+
+    csv_to_graph_read_write = [
+        "performance/csv/Ingi_temps_read_write_POSIX.csv",
+        "performance/csv/Ingi_temps_read_write_TAS.csv",
+        "performance/csv/Ingi_temps_read_write_TATAS.csv",
+    ]
+
+    # Names of the graphs
+    name_of_graph_philo = [
+        "Temps philo POSIX",
+        "Temps philo TAS",
+        "Temps philo TATAS",
+        "Temps du problème des philosophes en fonction du nombre de threads",
+    ]
+
+    name_of_graph_prod_cons = [
+        "Temps prod cons POSIX",
+        "Temps prod cons TAS",
+        "Temps prod cons TATAS",
+        "Temps du problème des producteurs-consommateurs en fonction du nombre de threads",
+    ]
+
+    name_of_graph_read_write = [
+        "Temps read write POSIX",
+        "Temps read write TAS",
+        "Temps read write TATAS",
+        "Temps du problème des lecteurs-écrivains en fonction du nombre de threads",
+    ]
+
+    # graph_line(csv_to_graph_philo, name_of_graph_philo, True)
+    # graph_line(csv_to_graph_prod_cons, name_of_graph_prod_cons, True)
+    graph_line(csv_to_graph_read_write, name_of_graph_read_write, True)
